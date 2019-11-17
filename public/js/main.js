35d5584a1b65dd9fc67fb9c5d465623c2b27aad4
haeseoklee <haeseoklee.dev@gmail.com>
2019-11-17 17:21:33 +0900

    파이어베이스 연동

---
+++ public/js/main.js
@@ -0,0 +1,126 @@
+
+/* ========================
+  Variables
+======================== */
+
+const FIREBASE_AUTH = firebase.auth();
+const FIREBASE_MESSAGING = firebase.messaging();
+const FIREBASE_DATABASE = firebase.database();
+const signInButton = document.getElementById('sign-in');
+const signOutButton = document.getElementById('sign-out');
+const subscribeButton = document.getElementById('subscribe');
+const unsubscribeButton = document.getElementById('unsubscribe');
+const sendNotificationForm = document.getElementById('send-notification-form');
+
+
+FIREBASE_AUTH.onAuthStateChanged(handleAuthStateChanged);
+FIREBASE_MESSAGING.onTokenRefresh(handleTokenRefresh);
+
+/* ========================
+  Event Listeners
+======================== */
+signInButton.addEventListener('click', signIn);
+signOutButton.addEventListener('click', signOut);
+subscribeButton.addEventListener('click', subscribeToNotifications);
+unsubscribeButton.addEventListener('click', unsubscribeFromNotifications);
+sendNotificationForm.addEventListener("submit", sendNotification);
+
+/* ========================
+  Functions
+======================== */
+function signIn() {
+  FIREBASE_AUTH.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+}
+
+function signOut() {
+  FIREBASE_AUTH.signOut();
+}
+
+function checkSubscription() {
+  FIREBASE_DATABASE.ref('/tokens').orderByChild("uid").equalTo(FIREBASE_AUTH.currentUser.uid).once('value').then((snapshot) => {
+    if ( snapshot.val() ) {
+      subscribeButton.setAttribute("hidden", "true");
+      unsubscribeButton.removeAttribute("hidden");
+    } else {
+      unsubscribeButton.setAttribute("hidden", "true");
+      subscribeButton.removeAttribute("hidden");
+    }
+  });
+}
+
+
+function subscribeToNotifications() {
+  FIREBASE_MESSAGING.requestPermission()
+  .then(() => handleTokenRefresh())
+  .then(() => checkSubscription())
+  .catch((err) => {
+    console.log("error getting permission :(");
+  });
+}
+
+function unsubscribeFromNotifications() {
+  FIREBASE_MESSAGING.getToken()
+    .then((token) => FIREBASE_MESSAGING.deleteToken(token))
+    .then(() => FIREBASE_DATABASE.ref('/tokens').orderByChild("uid").equalTo(FIREBASE_AUTH.currentUser.uid).once('value'))
+    .then((snapshot) => {
+      
+      const key = Object.keys(snapshot.val())[0];
+      console.log(key);
+      return FIREBASE_DATABASE.ref('/tokens').child(key).remove();
+    })
+    .then(() => checkSubscription())
+    .catch((err) => {
+      console.log("error deleting token :(");
+    });
+}
+
+function handleTokenRefresh() {
+  return FIREBASE_MESSAGING.getToken()
+    .then((token) => {
+      console.log(token);
+      FIREBASE_DATABASE.ref('/tokens').push({
+        token: token,
+        uid: FIREBASE_AUTH.currentUser.uid
+      });
+  });
+}
+
+
+function handleAuthStateChanged(user) {
+  if (user) {
+    // User is signed in
+    signInButton.setAttribute("hidden", "true");
+    signOutButton.removeAttribute("hidden");
+    subscribeButton.removeAttribute("hidden");
+    sendNotificationForm.removeAttribute("hidden");
+    checkSubscription();
+  } else {
+    // User is not signed in
+    console.log("user is not signed in");
+    signOutButton.setAttribute("hidden", "true");
+    signInButton.removeAttribute("hidden");
+    subscribeButton.setAttribute("hidden", "true");
+    unsubscribeButton.setAttribute("hidden", "true");
+    sendNotificationForm.setAttribute("hidden", "true");
+  }
+}
+
+function sendNotification(e) {
+  e.preventDefault();
+
+  const notificationMessage = document.getElementById('notification-message').value;
+  if ( !notificationMessage ) return;
+
+  FIREBASE_DATABASE.ref('/notifications')
+    .push({
+      user: FIREBASE_AUTH.currentUser.displayName,
+      message: notificationMessage,
+      userProfileImg: FIREBASE_AUTH.currentUser.photoURL
+    })
+    .then(() => {
+      document.getElementById('notification-message').value = "";
+    })
+    .catch(() => {
+      console.log("error sending notification :(")
+    });
+}
