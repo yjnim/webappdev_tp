431162aace5e7728755aeb24c544154163595391
YUNJI JANG <bee1513@gmail.com>
2019-11-25 01:16:13 +0900

    로그인페이지 완성

--- sel-front/src/routes/Login.js
+++ sel-front/src/routes/Login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './../css/Login.css'
+import Glogo from './../img/google-g-icon.jpg'
 
 class Login extends Component {
     //메인 화면
@@ -11,12 +12,22 @@ class Login extends Component {
                   <hr />
                   <form>
                     <div className="Login-input-div">
-                      <span className="Login-noti">ID</span>
+                      <span className="Login-title">ID</span>
                       <input type="text" className="Login-input"/>
                     </div>
                     <div className="Login-input-div">
-                      <span className="Login-noti">PW</span>
+                      <span className="Login-title">PW</span>
                       <input type="text" className="Login-input"/>
+                    </div>
+                    <div className="Login-button-div">
+                      <button type="submit" className="Login-button">
+                        Sign in
+                      </button>
+                    </div>
+                    <div className="Login-google-div">
+                      <button type="submit" className="Login-button">
+                        <img src={ Glogo } className="Glogo-img"></img>Sign in with Google
+                      </button>
                     </div>
                   </form>
               </div>
