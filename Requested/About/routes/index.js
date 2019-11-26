1247f221a6de24fbf1f669aa7219e70e38bbab7e
Libria <litaparit732@hanyang.ac.kr>
2019-11-26 22:14:00 +0900

    About 작업 완료

---
+++ Requested/About/routes/index.js
@@ -0,0 +1,4 @@
+export { default as Contact } from './Contact';
+export { default as Member } from './Member';
+export { default as Publications } from './Publications';
+export { default as Research } from './Research';
