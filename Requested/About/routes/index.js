f3f891059ca74d8e6962dbd35a0d6f68f340fd93
YUNJI JANG <bee1513@gmail.com>
2019-11-29 23:28:56 +0900

    react 폐기, js/jquery 전환

--- Requested/About/routes/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Contact } from './Contact';
-export { default as Member } from './Member';
-export { default as Publications } from './Publications';
-export { default as Research } from './Research';
