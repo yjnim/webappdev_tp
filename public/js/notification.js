b4e4ad1b271afcdfac29ac03bf8d8e85551623bc
YUNJI JANG <bee1513@gmail.com>
2019-12-17 23:47:17 +0900

    mypage, notification 수정

--- public/js/notification.js
+++ public/js/notification.js
@@ -12,9 +12,26 @@ sendNotificationForm.addEventListener("submit", sendNotification);
 
 FIREBASE_DATABASE.ref('/answers').on('child_added', function(data){
   let answers = data.val();
-  let p = document.createElement('p');
-  p.innerHTML = `${answers.name} : ${answers.answer}`
-  answerDiv.insertBefore(p, answerDiv.childNodes[0]);
+  // let p = document.createElement('p');
+  // p.innerHTML = `${answers.name} : ${answers.answer}`
+  // answerDiv.insertBefore(p, answerDiv.childNodes[0]);
+
+  let div = document.createElement('div');
+  div.setAttribute('class', 'a-answer');
+
+  for (var i=0; i<2; i++) {
+      let p = document.createElement('p');
+      if (i == 0) {
+          p.innerHTML = `${answers.name}`;
+          p.setAttribute('class', 'answer-name');
+      } else if (i == 1) {
+          p.innerHTML = `${answers.answer}`;
+          p.setAttribute('class', 'answer-contents');
+      }
+      div.appendChild(p);
+  }
+
+  answerDiv.insertBefore(div, answerDiv.childNodes[0]);
 })
 
 
